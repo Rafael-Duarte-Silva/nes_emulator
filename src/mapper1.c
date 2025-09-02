10c4cdc39bd770b1831c44364f38456f0b97febf
Rafael <38193394+Rafael-Duarte-Silva@users.noreply.github.com>
2025-09-01 21:21:14 -0300

    fix: comment

--- src/mapper1.c
+++ src/mapper1.c
@@ -24,5 +24,5 @@ ubyte read1(uint16_t address, cartrigde_t *cartrigde) {
 }
 
 void write1(uint16_t address, ubyte data, cartrigde_t *cartrigde) {
-    // teoricamente o pego os dados vindos por aqui e trato eles por registradores
+    // theoretically this function takes the data coming from here and processes it using registers
 }
